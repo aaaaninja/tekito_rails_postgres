247f05b54e7fc92f3a6e00d442c9be65db8bc76c
aaaaninja <keshigomu71505@gmail.com>
2019-08-04 18:44:40 +0900

    vueでcounter実装

---
+++ app/javascript/packs/counter_app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>Counter = {{ counter }}</div>
+    <button v-on:click="increment">おせ!! 私を！！</button>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return { counter: 0 }
+  },
+
+  methods: {
+    increment: function() {
+      this.counter++
+    }
+  }
+}
+</script>
